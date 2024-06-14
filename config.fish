feaf52b4966523108ac21d3b0046ad1b5169e1e8
Marcin Bałazy <mart@Marcins-Laptop.Dlink>
2024-06-14 16:16:40 +0200

    changes

--- config.fish
+++ config.fish
@@ -1,12 +1,21 @@
-# if status is-interactive
-#     # Commands to run in interactive sessions can go here
-# end
+set fish_greeting
+
+source $HOME/.config/fish/exports
+source $HOME/.config/fish/path
+source $HOME/.config/fish/aliases
 
 starship init fish | source
 zoxide init fish | source
+op completion fish | source
 
-set fish_greeting
+set -g fish_key_bindings fish_vi_key_bindings
 
-source ./path
-source ./exports
-source ./aliases
+# <C-t>
+bind \ct 'tmux attach || tmux ^M'
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# rvm
+# rvm default
